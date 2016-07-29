a68b0a4f01dc38a7bc792f0c1c623dcd79eb8ecf
Gabriel <Gabriel@Gabriel-Dell>
2016-07-29 12:17:25 -0300

    Definições das estruturas básicas (cor, pixel, linha e triangulo)

--- src/mygl.h
+++ src/mygl.h
@@ -3,10 +3,29 @@
 
 #include "definitions.h"
 
-//*****************************************************************************
-// Defina aqui as suas funções gráficas
-//*****************************************************************************
+typedef struct Color {
+	unsigned char R;
+	unsigned char G;
+	unsigned char B;
+	unsigned char A;
+} Color;
 
+typedef struct Pixel {
+	unsigned int x;
+	unsigned int y;
+	Color color;
+} Pixel;
+
+typedef struct Line {
+	Pixel pA;
+	Pixel pB;
+} Line;
+
+typedef struct Triangle {
+	Pixel vA;
+	Pixel vB;
+	Pixel vC;
+};
 
 
 
